d6277dcc9135cfa36ba26bd69e30643fa7bb28b8
Krishnanand Yadav <krishnanand2517@gmail.com>
2024-03-17 19:39:39 +0530

    Setup blog schema in Sanity

---
+++ sanity/schemaTypes/blogType.ts
@@ -0,0 +1,42 @@
+import {defineField, defineType} from 'sanity'
+
+export const blogType = defineType({
+  name: 'blog',
+  title: 'Blog',
+  type: 'document',
+  fields: [
+    defineField({
+      name: 'title',
+      type: 'string',
+      title: 'Title of blog article',
+    }),
+    defineField({
+      name: 'slug',
+      type: 'slug',
+      title: 'Slug of your blog article',
+      options: {
+        source: 'title',
+      },
+    }),
+    defineField({
+      name: 'titleImage',
+      type: 'image',
+      title: 'Title Image',
+    }),
+    defineField({
+      name: 'smallDescription',
+      type: 'text',
+      title: 'Small Description',
+    }),
+    defineField({
+      name: 'content',
+      type: 'array',
+      title: 'Content',
+      of: [
+        {
+          type: 'block',
+        },
+      ],
+    }),
+  ],
+})
